4c32fb786da3956b6c7623249f39c4165521b208
dimamarksman <dimamarksman@gmail.com>
2021-09-08 22:09:59 +0300

    fix(core): fix fd-calendar accessibility

--- libs/core/src/lib/calendar/calendar-directives.ts
+++ libs/core/src/lib/calendar/calendar-directives.ts
@@ -1,10 +1,24 @@
-import { Directive } from '@angular/core';
+import { Attribute, Directive } from '@angular/core';
+
+import { CalendarI18nLabels } from './i18n/calendar-i18n-labels';
 
 @Directive({
     // tslint:disable-next-line:directive-selector
     selector: '[fd-calendar-close-button]',
     host: {
-        '[class.fd-calendar__close-button]': 'true'
+        'class': 'fd-calendar__close-button',
+        '[attr.aria-label]': '_ariaLabel'
     }
 })
-export class CalendarCloseButtonDirective {}
+export class CalendarCloseButtonDirective {
+    /** @hidden */
+    get _ariaLabel(): string {
+        return this._originalAriaLabel || this._calendarI18nLabels.closeCalendarLabel;
+    }
+
+    /** @hidden */
+    constructor(
+        @Attribute('aria-label') private _originalAriaLabel: string,
+        private _calendarI18nLabels: CalendarI18nLabels
+    ) {}
+}
